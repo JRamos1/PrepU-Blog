f4fc5248edb4c1dd23a25888685627163665cea5
Justin Ramos <jmramos396@gmail.com>
2019-04-22 17:38:51 -0400

    fix

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,31 @@
 let db = require("../models");
 
 module.exports = function(app) {
+// Gets user information based on id param    
+    app.get("/api/users/:id", function (req, res){
+        db.User.findAll({
+            where:{
+                id: req.params.id
+            } 
+        }).then(function(results){
+            res.json(results)
+        });
+    });
+// Get all posts by a user
+    app.get("/api/posts/:user", function (req, res){
+        db.Post.findAll({
+            where:{
+                user: req.params.user
+            }
+        }).then(function(results){
+            res.json(results)
+        });
+    });
+// Takes user registration info and sends to "/api/users" route
+    app.post("/api/users", function(req, res){
+        db.User.create(req.body).then(function(results){
+            res.json(results)
+        });
+    });
 
 };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 
 const app = express()
 
-const PORT = process.env.port8080
+const PORT = process.env.PORT || 8080
 
 const db = require("./models")
 
