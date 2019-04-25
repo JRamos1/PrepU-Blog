8e36eddd6ddabbc604ef2d56c42c9c750a564fbd
Justin Ramos <jmramos396@gmail.com>
2019-04-24 20:24:59 -0400

    working

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 
-db.sequelize.sync({}).then(function(){
+db.sequelize.sync({force: true}).then(function(){
     app.listen(PORT, function(){
         console.log("App listening on PORT " + PORT);
     });
